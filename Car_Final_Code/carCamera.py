0371a6b80bc9147e0049c568d58366064c29de38
Matthew Bush <tucker15@gmail.com>
2019-10-21 18:31:29 -0800

    add camera

---
+++ Car_Final_Code/carCamera.py
@@ -0,0 +1,22 @@
+from picamera import PiCamera
+import time
+import os
+
+class CarCamera():
+
+    def __init__(self):
+        self.camera = PiCamera()
+        self.camera.resolution = ('720p')
+        self.camera.framerate = 24
+
+    def start(self):
+        os.chdir("/home/pi/Desktop/Car/videos")
+        start_time = (time.time())
+        self.camera.start_recording("{0:f}".format(start_time), format = 'h264')
+        return float(time.time())
+
+
+    def stop(self):
+        if(self.camera.recording):
+            self.camera.stop_recording()
+            return float(time.time())
